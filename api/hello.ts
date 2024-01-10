28b56a1825c275b0db8c8754d40d12e8d9e92729
Evan Shortiss <evanshortiss@gmail.com>
2024-01-09 21:02:36 -0800

    return postgres version

--- api/hello.ts
+++ api/hello.ts
@@ -1,8 +1,10 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
+import { neon } from '@neondatabase/serverless';
+const sql = neon(process.env.DATABASE_URL!);
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  const { name = 'World' } = req.query
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  const version = await sql`SELECT version();`
   return res.json({
-    message: `Hello ${name}!`,
+    version
   })
 }
