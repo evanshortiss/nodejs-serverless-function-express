66c4ee236249ee7360b82326b0b1c828a4eeead6
Evan Shortiss <evanshortiss@gmail.com>
2024-02-22 18:19:18 -0800

    Update hello.ts

--- api/hello.ts
+++ api/hello.ts
@@ -7,6 +7,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   const rows = await sql`SELECT * from playing_with_neon;`
   const version = await sql`SELECT version();`
   return res.json({
+    hostname: new URL(process.env.DATABASE_URL).hostname,
     version,
     rows
   })
