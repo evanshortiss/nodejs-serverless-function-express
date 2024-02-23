6380b07b1ac87b5a0f1861fdfa593edf9725123b
Evan Shortiss <evanshortiss@gmail.com>
2024-02-22 18:20:35 -0800

    Update hello.ts

--- api/hello.ts
+++ api/hello.ts
@@ -1,9 +1,7 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 import { neon } from '@neondatabase/serverless';
-
-export default async function handler(req: VercelRequest, res: VercelResponse) {
-  const sql = neon(process.env.DATABASE_URL);
-  
+const sql = neon(process.env.DATABASE_URL);
+export default async function handler(req: VercelRequest, res: VercelResponse) {  
   const rows = await sql`SELECT * from playing_with_neon;`
   const version = await sql`SELECT version();`
   return res.json({
